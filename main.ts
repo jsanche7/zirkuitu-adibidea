62e23e7984285a0bab4b782f923f48ba660c6611
jsanche7 <80990132+jsanche7@users.noreply.github.com>
2022-05-25 14:40:29 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,31 @@
 DFRobotMaqueenPlus.I2CInit()
 let _1_atala = 1
 let _2_atala = 0
+let _3_atala = 0
 basic.forever(function () {
     if (DFRobotMaqueenPlus.readPatrol(Patrol.R3) == 1) {
-        _1_atala = 0
-        _2_atala = 1
-    }
-})
-basic.forever(function () {
-    if (_2_atala == 1) {
-        basic.showIcon(IconNames.Heart)
-        if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 30) {
-            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
-        } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 15) {
-            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 30)
-        } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 8) {
-            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 25)
-        } else {
-            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 0)
-            basic.showIcon(IconNames.Heart)
-        }
+    	
     }
 })
 basic.forever(function () {
     if (_1_atala == 1) {
+        basic.showNumber(1)
+        if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 30) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
+        } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 15) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 30)
+        } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 10) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 25)
+        } else {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 0)
+            basic.showIcon(IconNames.Heart)
+            _1_atala = 0
+            _2_atala = 1
+        }
+    }
+})
+basic.forever(function () {
+    if (_3_atala == 1) {
         if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
             DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 30)
         } else {
@@ -36,5 +38,16 @@ basic.forever(function () {
                 DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 160)
             }
         }
+    }
+})
+basic.forever(function () {
+    if (_2_atala == 1) {
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 37)
+        basic.pause(1500)
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        basic.showNumber(2)
+        basic.pause(1000)
+        _2_atala = 0
+        _3_atala = 1
     }
 })
